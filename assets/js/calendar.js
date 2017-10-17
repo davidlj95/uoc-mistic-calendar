4d937ca03c3dcadd23d33d2ad8903a91cfceba98
David LJ <mail@davidlj95.com>
2017-10-17 02:09:14 +0200

    First version with sorted table automatically

---
+++ assets/js/calendar.js
@@ -0,0 +1,54 @@
+/** when ready, load table **/
+$( document ).ready(function() {
+    $("table").tablesorter({sortList: [[3,0]]});
+    load_table();
+});
+
+function load_table() {
+    // get events
+    $.get("events.csv", null, loaded_events)
+}
+
+function loaded_events(data, status, jqxhr) {
+    data = data.split("\n");
+    csv = []
+    // transform each row into an array
+    for (i = 0; i < data.length; i++) {
+        csv.push(data[i].split(","))
+    }
+    // remove invalid columns
+    for (i = 0; i < csv.length; i++) {
+        if(csv[i].length < 4) {
+            csv.splice(i, 1)
+        }
+    }
+    // remove header
+    csv.splice(0, 1)
+    // add to table
+    add_data(csv);
+}
+
+function add_data(data) {
+    // find table
+    table = $("table");
+    table_body = table.find("tbody")
+    data.forEach(function(el){
+        row = $("<tr class='addedRows'>")
+        // add info
+        el.forEach(function(item) {
+            row.append($("<td>").append(item))
+        })
+        // add duration
+        row.append($("<td>").append("undefined bruh"))
+        // add row
+        table_body.append(row)
+    })
+    var config = $( 'table' )[ 0 ].config,
+      $rows = $( 'tr.addedRows' ), // jQuery selector of newly appended row(s)
+      // applies or reapplies a sort to the table; use false to not update the sort
+      resort = true, // or [ [0,0], [1,0] ] etc
+      callback = function( table ) {
+        // do something
+      };
+      $.tablesorter.addRows( config, $rows, resort, callback );
+}
